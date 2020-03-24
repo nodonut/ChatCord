6c8d1e7b5df70276f6a5f0ac97e89a2a69a410f3
Apoorv Sohal <apoorvsohal@protonmail.com>
2020-03-24 21:15:44 +0530

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,70 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessage = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+});
+
+console.log(username, room);
+
+const socket = io();
+
+// Join a chatroom
+socket.emit('joinRoom', { username, room });
+
+// Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
+// Message  from server
+socket.on('message', message => {
+  console.log(message);
+  outputMessage(message);
+
+  // Scroll down
+  chatMessage.scrollTop = chatMessage.scrollHeight;
+});
+
+// Message submit
+chatForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // Get message text
+  const msg = e.target.elements.msg.value;
+
+  // Emit message to server
+  socket.emit('chatMessage', msg);
+
+  // Clear input
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
+
+// Output message to DOM
+function outputMessage(message) {
+  const chatMessageDiv = document.createElement('div');
+
+  chatMessageDiv.classList.add('message');
+  chatMessageDiv.innerHTML += `<p class="meta">${message.username} <span>${message.time}</span></p>
+  <p class="text">
+    ${message.text}
+  </p>`;
+  chatMessage.appendChild(chatMessageDiv);
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+  userList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join('')}
+  `;
+}
